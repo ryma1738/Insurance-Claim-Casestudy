3d6eb4352ddf4470ae4d54299db446feec285506
Ryan Jepson <RyanJepson.webdev@gmail.com>
2022-10-12 09:11:11 -0600

    inital files

---
+++ src/main/java/com/auto/insuranceClaim/InsuranceClaimApplication.java
@@ -0,0 +1,13 @@
+package com.auto.insuranceClaim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceClaimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsuranceClaimApplication.class, args);
+	}
+
+}
